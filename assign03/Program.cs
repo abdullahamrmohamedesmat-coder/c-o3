08cf7480861e050102f37373d85ad6632a91b330
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-08-30 23:41:17 +0300

    Add project files.

---
+++ assign03/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace assign03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region first question
+            //1st answer
+            int pages = 464;
+            bool isAvailable = true;
+
+            if (pages > 300 && isAvailable == true)
+            {
+
+                Console.WriteLine("you can borrow this book");
+            }
+            #endregion
+
+        }
+    }
+}
